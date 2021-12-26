c5a179cafff231e95e86803242cf94922d0fb1e1
GragonCat <475971563@qq.com>
2021-12-26 18:47:28 +0800

    a

--- src/main/java/com/netty/HelloClient.java
+++ src/main/java/com/netty/HelloClient.java
@@ -1,6 +1,7 @@
 package com.netty;
 
 import io.netty.bootstrap.Bootstrap;
+import io.netty.channel.Channel;
 import io.netty.channel.ChannelInitializer;
 import io.netty.channel.nio.NioEventLoopGroup;
 import io.netty.channel.socket.nio.NioServerSocketChannel;
@@ -12,20 +13,27 @@ import java.net.InetSocketAddress;
 public class HelloClient {
 
     public static void main(String[] args) throws InterruptedException {
-        new Bootstrap()
+        //客户端启动器
+        Channel channel = new Bootstrap()
+                // 添加循环事件组 监听事件
                 .group(new NioEventLoopGroup())
+                //绑定socketChannel类型
                 .channel(NioSocketChannel.class)
+                //绑定channel初始化器
                 .handler(new ChannelInitializer<NioSocketChannel>() {
-
+                    //channel建立时调用
                     @Override
                     protected void initChannel(NioSocketChannel nioSocketChannel) throws Exception {
+                        //添加编码器
                         nioSocketChannel.pipeline().addLast(new StringEncoder());
                     }
                 })
-                .connect(new InetSocketAddress("localhost",8080))
-                .sync()
-                .channel()
-                .writeAndFlush("Hello,This is my first netty demo");
+                //建立连接
+                .connect(new InetSocketAddress("localhost", 8080))
+                .sync()//阻塞线程，直到连接建立
+                .channel();
+                //.writeAndFlush("hello...");
+        System.out.println();
 
     }
 }
--- src/main/java/com/netty/HelloServer.java
+++ src/main/java/com/netty/HelloServer.java
@@ -14,22 +14,30 @@ import io.netty.handler.codec.string.StringEncoder;
 public class HelloServer {
 
     public static void main(String[] args) {
+        //1 服务端启动器
         new ServerBootstrap()
+                // 添加循环事件组 EventLoopGroup（BOSS,WORKER），服务端添加Boss，监听accept、read等事件
                 .group(new NioEventLoopGroup())
+                //绑定服务端 NioServerSocketChannel 类型
                 .channel(NioServerSocketChannel.class)
+                //Boss负责建立连接，worker(child)负责处理读写
                 .childHandler(
+                        // 添加channel初始化器
                         new ChannelInitializer<NioSocketChannel>() {
+                            // 客户端连接时 调用initChannel方法，初始化channel
                             @Override
                             protected void initChannel(NioSocketChannel nioSocketChannel) throws Exception {
-                                nioSocketChannel.pipeline().addLast(new StringDecoder());
+                                //pipeLine 管道，类似过滤器，收到的数据进行处理
+                                nioSocketChannel.pipeline().addLast(new StringDecoder());//解码器
+                                //处理数据read事件
                                 nioSocketChannel.pipeline().addLast(new ChannelInboundHandlerAdapter(){
-
                                     @Override
                                     public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
                                         System.out.println(msg);
                                     }
                                 });
                             }
+                            //绑定端口
                 }).bind(8080);
     }
 }
